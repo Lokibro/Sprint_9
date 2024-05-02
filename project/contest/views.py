539ced7263d724367ef31f35476472b5793abcff
Roman Nikitin <ya@fagundes.ru>
2024-05-02 18:07:50 +0300

    study

--- project/contest/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def proposal_create(request):
-    return render(request, 'contest/form.html')
-
-
-def accepted(request):
-    return render(request, 'contest/proposal_accepted.html')
