1474c8fb0899057de3cc5cb1348d924d131ec1db
Roman Nikitin <ya@fagundes.ru>
2024-05-03 17:56:12 +0300

    study

---
+++ anfisa_project/anfisa_project/contest/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class Contest(models.Model):
+    title = models.CharField(
+        verbose_name='Название',
+        max_length=20
+    )
+    description = models.TextField(
+        verbose_name='Описание'
+    )
+    price = models.IntegerField(
+        verbose_name='Цена',
+        help_text='Рекомендованная розничная цена',
+        validators=[MinValueValidator(10), MaxValueValidator(100)]
+    )
+    comment = models.TextField(
+        verbose_name='Комментарий',
+        blank=True
+    )
